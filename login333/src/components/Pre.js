98c5759a6256798647c58ebc05e93d1ae37be94e
Tamonwan-Promthong <123473240+Tamonwan-Promthong@users.noreply.github.com>
2023-05-23 00:11:31 +0700

    Add files via upload

---
+++ login333/src/components/Pre.js
@@ -0,0 +1,18 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// const Pre = () => {
+//     return (
+//       <div class="main">
+//           <div class="signup">
+  
+//       <label for="chk" aria-hidden="true">EV BUS</label>
+//    <button type="submit">Sign Up</button>
+//      <button type="submit">Log in</button>
+
+//         </div>
+  
+//     </div>
+//       );
+
+// }
+// export default Pre;
